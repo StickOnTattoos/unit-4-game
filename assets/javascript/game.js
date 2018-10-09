1138bc2e1618a510f42fb131eb4858c5fee9c5c6
Tyler Bumguardner <tylerbumguardner@gmail.com>
2018-10-09 06:48:11 -0700

    added jquery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,77 +1,103 @@
 var startingNumber = Math.floor(Math.random() * 108) + 12;
 var wins = 0;
 var losses = 0;
-var playerTotal = 0;
-var winner = false;
-var loser = false;
+var playerTotal_1 = 0;
+var playerTotal_2 = 0;
+var reset = false;
+var crystalZero = 0;
+var crystalArray = [];
+var gem1 = crystalArray[0];
+var gem2 = crystalArray[1];
+var gem3 = crystalArray[2];
+var gem4 = crystalArray[3];
+console.log(crystalArray);
+crystalNumber;
 
-console.log(startingNumber);
-console.log(wins);
 
-
-// $("#screenGuess").html("hello");
-// $(".screenGuess").html("#" + startingNumber);
-// document.getElementById('screenGuess').innerHTML += "#" + startingNumber;
-
-
-// random 1-12 generator
-function crystalGame () {
-
-}
 
 // create 4 random numbers between 1-12 and apply to crystals
-
 for (var i = 0; i < 4; i++) {
     var crystalNumber = Math.floor(Math.random() * 11) + 1;
-    console.log(crystalNumber);
-    // apply to crystals
+    crystalArray.push(crystalNumber);
 }
+
+var gems = {
+    blue: {
+        value: crystalArray[0],
+    },
+    orange: {
+        value: crystalArray[1],
+    },
+    red: {
+        value: crystalArray[2],
+    },
+    green: {
+        value: crystalArray[3],
+    },
+}
+
+//gem buttons
+$(document).ready(function () {
+
+    $("#gem1").click(function () {
+        var playerTotal_1 = this.value = crystalArray[0];
+        console.log(playerTotal_1);
+        (playerTotal_1 + playerTotal_2);
+    });
+
+    $("#gem2").click(function () {
+        var playerTotal_1 = this.value = crystalArray[1];
+        console.log(playerTotal_1);
+        (playerTotal_1 + playerTotal_2);
+    });
+
+    $("#gem3").click(function () {
+        var playerTotal_1 = this.value = crystalArray[2];
+        console.log(playerTotal_1);
+        (playerTotal_1 + playerTotal_2);
+    });
+
+    $("#gem4").click(function () {
+        var playerTotal_1 = this.value = crystalArray[3];
+        console.log(playerTotal_1);
+        (playerTotal_1 + playerTotal_2);
+    })
+});
 
 // score keeper
 function scorekeeper() {
-    if (startingNumber = playerTotal) {
+    if (startingNumber = playerTotal_2) {
         wins++;
-        winner = true;
+        $("#Choosey").text("WINNER!!");
+        reset = true;
+        // reset function
     } else {
         (startingNumber < playerTotal)
         losses++;
-        loser = true;
+        $("#Choosey").text("LOSER!");
+        reset = true;
         // reset function
     }
 }
 
-// reset function
+// reset function that resets 10 seconds after game is won or lost
 function resetGame() {
-    if (winner = true);
-    // display winner and reset crystal 1-12 numbers
-    else (loser = true);
-    // display loser and reset crystal 1-12 numbers
-    
+    if (reset = true) {
+        setTimeOut(resetGame1, 10 * 1000);
+    }};
+ 
+function resetGamer1() {
+    // reset crystal values
 }
 
 
+$("#winnerBoard").html("Wins: " +wins);
+$("#loserBoard").html("Losses: "+losses);
+$("#screenGuess").html("#" + startingNumber);
+$("#scoreTotalVar").html(playerTotal_2);
 
 
-// click functions
-$(".crystal").on("click", function () {
-    var num = parseINT($(this).attr('data'));
-    console.log(crystalNumber);
-    
-    
-});
-console.log($("this"));
-
-// $(document).ready(function () {
-//     $(".crystal").on("click", function  () {
-//         $(".crystal").attr(randomNumber)
-//     console.log($("this"));
-//     });
-//   });
-
-
-
-
-// $(".crystal").on(click, function () {
-//     console.log($(this));
+// monday notes  this is the javascript way to get value off a button
+// $(".btn").on("click", function(e){ 
+// var vanillaVal= this.value; 
 // });
-
